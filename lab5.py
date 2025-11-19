5e59910237fe5aef544595f849fff916e53ab96b
Грудев Александр <gtvegas35@gmail.com>
2025-11-19 17:03:22 +0700

    fix

--- lab5.py
+++ lab5.py
@@ -34,13 +34,14 @@ def db_connect():
             # Если PostgreSQL недоступен, автоматически переключаемся на SQLite
             print("PostgreSQL недоступен, используется SQLite")
             db_type = 'sqlite'
-    
+
     # Используем SQLite
     dir_path = path.dirname(path.realpath(__file__))
     db_path = path.join(dir_path, "database.db")
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
+    return conn, cur
 
 
 def db_close(conn, cur):
