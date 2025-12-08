d5baa088470aa6881b759b665bcafbb3aa5c2fba
Грудев Александр <gtvegas35@gmail.com>
2025-12-08 13:37:21 +0300

    Проверка описания

--- lab7.py
+++ lab7.py
@@ -93,6 +93,8 @@ def del_film(id):
 def put_film(id):
     if 0 <= id < len(films):
         film = request.get_json()
+        if film['description'] == '':
+            return {'description': 'Заполните описание'}, 400
         films[id] = film
         return films[id], 200
     else:
@@ -102,6 +104,8 @@ def put_film(id):
 @lab7.route('/lab7/rest-api/films/', methods=['POST'])
 def add_film():
     film = request.get_json()
+    if film.get('description', '') == '':
+        return {'description': 'Заполните описание'}, 400
     films.append(film)
     new_index = len(films) - 1
     return {"id": new_index, "film": film}, 201
